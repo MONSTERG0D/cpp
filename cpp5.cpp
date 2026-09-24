3ac1c7e947fe3913752b60f2d1d0305d573bb8a0
AAMIR <AS3498558@GMAIL.COM>
2026-09-24 08:41:49 +0530

    Add inverted triangle pattern

---
+++ cpp5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int n = 4;
+    for (int i=0;i<n;i++){
+        for (int k=0;k<=i;k++){
+            cout << " ";
+            
+        }
+        
+        for (int j=n-i;j>0;j--){
+            
+        cout << i+1;
+        }
+        cout << endl;
+    }
+    return 0;
+}
