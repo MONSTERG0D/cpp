6908d20a7c7276887ef8ccf1c540a18900990ae9
AAMIR <AS3498558@GMAIL.COM>
2026-09-24 07:49:32 +0530

    alphabetical pattarns

--- cpp2.cpp
+++ cpp2.cpp
@@ -4,8 +4,10 @@ using namespace std;
 int main (){
     int n = 4;
     for (int i =0;i<n;i++){
+        char ch = 'a';
         for (int j =0 ;j<n;j++){
-            cout << j ;
+            cout << ch ;
+            ch = ch + 1;
         }
         cout << endl;
     }
